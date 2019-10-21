6ead0d713525a0b2e886898296a6cb22c64fc86f
ASHLEY_MILES_TAYLOR <ASHLEY_MILES_TAYLOR@GMAIL.COM>
2019-10-21 16:19:37 -0400

    red vs blue works

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-const box = document.querySelectorAll(".box")
+let redOrBlue = 0
+const box = document.querySelectorAll(".box");
+
 for (let i = 0; i < box.length; i++) {
 	box[i].addEventListener("click", function(evt) {
 		evt.preventDefault();
 		console.log(evt);
 		console.log("box")
 		// const boxcolor = document.querySelectorAll(".box");
-		box[i].style.background = "red";
+		if ( redOrBlue % 2 === 1) {
+			this.style.background = "red"
+			redOrBlue = redOrBlue +=1;
+		} else if ( redOrBlue % 2 === 0) {
+				this.style.background = "blue"	
+				redOrBlue = redOrBlue +=1;
+		} else {
+			console.log("tubular")
+		}
 	})
 }
+
+// let redOrBlue = 0
+// redOrBlue = redOrBlue +=1;
